cbd5d6a11c340d3f0bf4ff0469d036414bb08b8b
Owen Larson <owenslar@byu.edu>
2024-10-29 19:48:14 -0600

    finished navbar, collapse works

--- src/app.jsx
+++ src/app.jsx
@@ -9,62 +9,62 @@ import { About } from './about/about';
 
 export default function App() {
   return (
-  <BrowserRouter>
-  <div>
-    <header>
-        <nav className="navbar navbar-expand-md bg-primary navbar-dark">
+    <BrowserRouter>
+      <div>
+        <header>
+          <nav className="navbar navbar-expand-md bg-primary navbar-dark">
             <div className="container-fluid">
-                <NavLink className="navbar-brand" to="">
-                    <img src="favicon-2.ico" alt="logo" style={{ width: '30px', height: '30px' }} />
-                    BookATeeTime<sup>&reg;</sup>
-                </NavLink>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNavbar" aria-controls="collapseNavbar" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="collapseNavbar">
-                    <ul className="navbar-nav me-auto">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to=''>Login</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="book">Book a Tee Time</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="data">Your Reservations</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="about">About</NavLink>
-                        </li>
-                    </ul>
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="">Account</NavLink>
-                        </li>
-                    </ul>
-                </div>
+              <NavLink className="navbar-brand" to="">
+                <img src="favicon-2.ico" alt="logo" style={{ width: '30px', height: '30px' }} />
+                BookATeeTime<sup>&reg;</sup>
+              </NavLink>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNavbar" aria-controls="collapseNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="collapseNavbar">
+                <ul className="navbar-nav me-auto">
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to=''>Login</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="book">Book a Tee Time</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="data">Your Reservations</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="about">About</NavLink>
+                  </li>
+                </ul>
+                <ul className="navbar-nav ms-auto">
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="">Account</NavLink>
+                  </li>
+                </ul>
+              </div>
             </div>
-        </nav>
-    </header>
+          </nav>
+        </header>
 
-    <Routes>
-      <Route path='/' element={<Login />} exact />
-      <Route path='/book' element={<Book />} />
-      <Route path='/data' element={<Data />} />
-      <Route path='/about' element={<About />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+        <Routes>
+          <Route path='/' element={<Login />} exact />
+          <Route path='/book' element={<Book />} />
+          <Route path='/data' element={<Data />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
 
-    <footer className="bg-primary text-white-50">
-        <div className="container-fluid">
+        <footer className="bg-primary text-white-50">
+          <div className="container-fluid">
             <span className="text-reset">Owen Larson</span>
             <a className="text-reset" href="https://github.com/owenslar/startup2024">GitHub Repository</a>
-        </div>
-    </footer>
-  </div>
-  </BrowserRouter>
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
 function NotFound() {
-  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+  return <h2>Page not found</h2>;
 }
