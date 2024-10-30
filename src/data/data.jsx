05baa371edc809f03399fcdcf14fb6d7f9bdca55
Owen Larson <owenslar@byu.edu>
2024-10-29 19:18:44 -0600

    create view components

---
+++ src/data/data.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Data() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>data displayed here</div>
+    </main>
+  );
+}
