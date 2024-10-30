5e9742132df8f7207b9c5d9860abf6c992acefa3
Owen Larson <owenslar@byu.edu>
2024-10-29 20:10:09 -0600

    converted data and login components

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import './data.css';
 
 export function Data() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>data displayed here</div>
+    <main className="container-fluid text-center bg-secondary">
+        <div className="text-right text-primary">
+            Signed in as:
+            <span className="text-right text-primary">Username</span>
+        </div>
+        <h2 className="text-primary">Your Reservation History</h2> <br />
+            <div className="container-fluid text-center table-wrapper">
+                <table className="table table-striped-columns border text-primary">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Status</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Location</th>
+                            <th>Edit Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Reserved</td>
+                            <td>October 5, 2024</td>
+                            <td>10:05 am</td>
+                            <td><a href="https://www.sleepyridgegolf.com">The Links At Sleepy Ridge, Orem, Utah</a></td>
+                            <td><button type="button" className="btn btn-primary">Change</button></td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Reserved</td>
+                            <td>October 6, 2024</td>
+                            <td>12:15 pm</td>
+                            <td><a href="https://www.riversidecountryclub.org">Riverside Country Club, Provo, Utah</a></td>
+                            <td><button type="button" className="btn btn-primary">Change</button></td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Cancelled</td>
+                            <td>September 28, 2024</td>
+                            <td>9:30 am</td>
+                            <td><a href="https://www.timpanogosgolf.com">Timpanogos Golf Club, Provo, Utah</a></td>
+                            <td><button type="button" className="btn btn-primary">Change</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
     </main>
   );
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+    <main className="container-fluid text-center bg-secondary">
+        <div className="login">
+            <h1 className="text-primary">Login</h1>
+            <form method="get" action="book.html">
+                <div className="input-group mb-3">
+                    <span className="input-group-text">Email</span>
+                    <input className="form-control" type="text" placeholder="your@email.com" />
+                </div>
+                <div className="input-group mb-3">
+                    <span className="input-group-text">Password</span>
+                    <input className="form-control" type="password" placeholder="password" />
+                </div>
+                <button type="submit" className="btn btn-primary">Login</button>
+                <button type="submit" className="btn btn-secondary">Create</button>
+            </form>
+        </div>
     </main>
   );
 }
