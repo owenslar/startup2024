517e87eda6ae8a7a5b6184d4308bdf87f7b08fb9
Owen Larson <owenslar@byu.edu>
2024-10-29 20:00:05 -0600

    converted about and book components

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './about.css';
 
 export function About() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>about displayed here</div>
+    <main className="container-fluid bg-secondary text-center">
+        <h3 className="text-primary about-top">About</h3>
+        <div className="container">
+            <p className="text-primary text-left">BookATeeTime is a website meant to make finding and reserving tee times from any course in your area very easy. The goal is to create a one stop shop where you can find any tee time in the area and reserve it all from this website. (Disclaimer: These tee times are fake, do not show up to these golf courses and expect to have a reserved tee time.)</p>
+        </div>
     </main>
   );
 }
--- src/book/book.jsx
+++ src/book/book.jsx
@@ -1,9 +1,107 @@
 import React from 'react';
+import './book.css';
 
 export function Book() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>book displayed here</div>
+    <main className="container-fluid text-center bg-secondary">
+        <div className="text-right text-primary">
+            Signed in as:
+            <span className="text-right text-primary">Username</span>
+        </div>
+        <h2 className="text-primary">Available Tee Times</h2>
+        <div className="container mt-3">
+            <div className="card" style={{ width: '300px' }}>
+                <div style={{ height: '300px', objectFit: 'cover' }}>
+                    <img className="card-img-top img-fluid" alt="timpanogosGCimg" src="https://www.timpanogosgolf.com/wp-content/uploads/2022/01/Photo-Dec-05-8-30-41-PM-scaled.jpg" />
+                </div>
+                <div className="card-body">
+                    <h4 className="card-title">Timpanogos Golf Club</h4>
+                    <p className="card-text">Tomorrow, 9:30am</p>
+                    <p className="card-text">$40</p>
+                    <p className="card-text">2-4 golfers</p>
+                    <p className="card-text">Current Weather: Fair</p>
+                    <input type="button" value="Book" class="btn btn-primary" />
+                </div>
+            </div>
+            <div className="card" style={{ width: '300px' }}>
+                <img className="card-img-top img-fluid" alt="linksatsleepyridgeimg" src="https://golfcoursegurus.com/photos/utah/sleepyridge/large/Sleepy-Ridge-clubhouse-mountain.jpg" />
+                <div className="card-body">
+                    <h4 className="card-title">The Links at Sleepy Ridge</h4>
+                    <p className="card-text">Tomorrow, 10:35am</p>
+                    <p className="card-text">$55</p>
+                    <p className="card-text">1 golfer</p>
+                    <p className="card-text">Current Weather: Fair</p>
+                    <input type="button" value="Book" class="btn btn-primary" />
+                </div>
+            </div>
+            <div className="card" style={{ width: '300px' }}>
+                <img className="card-img-top img-fluid" alt="riversideCCimg" src="https://media.licdn.com/dms/image/v2/C4D1BAQFmAYoU9h3HFg/company-background_10000/company-background_10000/0/1625267164271/riverside_country_club_provo_cover?e=2147483647&v=beta&t=gdsiAv_j3bA-YADzbZG98aX5b8HJ4d0Fqs79fy__fw4" />
+                <div className="card-body">
+                    <h4 className="card-title">Riverside Country Club</h4>
+                    <p className="card-text">Tomorrow, 12:45pm</p>
+                    <p className="card-text">$65</p>
+                    <p className="card-text">2-4 golfers</p>
+                    <p className="card-text">Current Weather: Fair</p>
+                    <input type="button" value="Book" class="btn btn-primary" />
+                </div>
+            </div>
+            <div className="card" style={{ width: '300px' }}>
+                <img className="card-img-top img-fluid" alt="riversideCCimg" src="https://media.licdn.com/dms/image/v2/C4D1BAQFmAYoU9h3HFg/company-background_10000/company-background_10000/0/1625267164271/riverside_country_club_provo_cover?e=2147483647&v=beta&t=gdsiAv_j3bA-YADzbZG98aX5b8HJ4d0Fqs79fy__fw4" />
+                <div className="card-body">
+                    <h4 className="card-title">Riverside Country Club</h4>
+                    <p className="card-text">Tomorrow, 1:15pm</p>
+                    <p className="card-text">$65</p>
+                    <p className="card-text">2-4 golfers</p>
+                    <p className="card-text">Current Weather: Fair</p>
+                    <input type="button" value="Book" className="btn btn-primary" />
+                </div>
+            </div>
+            <div className="card" style={{ width: '300px' }}>
+                <img className="card-img-top img-fluid" alt="timpanogosGCimg" src="https://www.timpanogosgolf.com/wp-content/uploads/2022/01/Photo-Dec-05-8-30-41-PM-scaled.jpg" />
+                <div className="card-body">
+                    <h4 className="card-title">Timpanogos Golf Club</h4>
+                    <p className="card-text">Tomorrow, 2:00pm</p>
+                    <p className="card-text">$40</p>
+                    <p className="card-text">2-4 golfers</p>
+                    <p className="card-text">Current Weather: Fair</p>
+                    <input type="button" value="Book" className="btn btn-primary" />
+                </div>
+            </div>
+            <div className="card" style={{ width: '300px' }}>
+                <img className="card-img-top img-fluid" alt="linksatsleepyridgeimg" src="https://golfcoursegurus.com/photos/utah/sleepyridge/large/Sleepy-Ridge-clubhouse-mountain.jpg" />
+                <div className="card-body">
+                    <h4 className="card-title">The Links at Sleepy Ridge</h4>
+                    <p className="card-text">Tomorrow, 2:05pm</p>
+                    <p className="card-text">$55</p>
+                    <p className="card-text">2-4 golfers</p>
+                    <p className="card-text">Current Weather: Fair</p>
+                    <input type="button" value="Book" className="btn btn-primary" />
+                </div>
+            </div>
+            <div className="card" style={{ width: '300px' }}>
+                <img className="card-img-top img-fluid" alt="riversideCCimg" src="https://media.licdn.com/dms/image/v2/C4D1BAQFmAYoU9h3HFg/company-background_10000/company-background_10000/0/1625267164271/riverside_country_club_provo_cover?e=2147483647&v=beta&t=gdsiAv_j3bA-YADzbZG98aX5b8HJ4d0Fqs79fy__fw4" />
+                <div className="card-body">
+                    <h4 className="card-title">Riverside Country Club</h4>
+                    <p className="card-text">Tomorrow, 3:45pm</p>
+                    <p className="card-text">$40</p>
+                    <p className="card-text">1-2 golfers</p>
+                    <p className="card-text">Current Weather: Fair</p>
+                    <input type="button" value="Book" className="btn btn-primary" />
+                </div>
+            </div>
+            <div className="card" style={{ width: '300px' }}>
+                <img className="card-img-top img-fluid" alt="timpanogosGCimg" src="https://www.timpanogosgolf.com/wp-content/uploads/2022/01/Photo-Dec-05-8-30-41-PM-scaled.jpg" />
+                <div className="card-body">
+                    <h4 className="card-title">Timpanogos Golf Club</h4>
+                    <p className="card-text">Tomorrow, 6:30pm</p>
+                    <p className="card-text">$35</p>
+                    <p className="card-text">1-2 golfers</p>
+                    <p className="card-text">Current Weather: Fair</p>
+                    <input type="button" value="Book" className="btn btn-primary" />
+                </div>
+            </div>
+        </div>
+        <br />
     </main>
   );
 }
