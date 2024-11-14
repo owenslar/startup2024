33ca2923efb3c70bc0c48d2cafc6c25efee9f889
Owen Larson <owenslar@byu.edu>
2024-11-14 14:53:57 -0700

    service login and create endpoints

--- service/index.js
+++ service/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+app.use(express.static('public'));
 
-app.get('*', (_req, res) => {
-  res.send({ msg: 'BookATeeTime service' });
-});
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
@@ -14,3 +12,40 @@ app.listen(port, () => {
 const uuid = require('uuid');
 
 app.use(express.json());
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+apiRouter.post('/auth/create', async (req, res) => {
+    const user = users[req.body.email];
+    if (user) {
+      res.status(409).send({ msg: 'Existing user' });
+    } else {
+      const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
+      users[user.email] = user;
+  
+      res.send({ token: user.token });
+    }
+});
+  
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = users[req.body.email];
+    if (user) {
+      if (req.body.password === user.password) {
+        user.token = uuid.v4();
+        res.send({ token: user.token });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+});
+  
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', (req, res) => {
+    const user = Object.values(users).find((u) => u.token === req.body.token);
+    if (user) {
+      delete user.token;
+    }
+    res.status(204).end();
+});
