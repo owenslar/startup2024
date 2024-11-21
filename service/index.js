cf5afbd7c7a579f28ed84f0e5be68e8ca4559170
Owen Larson <owenslar@byu.edu>
2024-11-21 16:38:56 -0700

    removed tee times from index.js

--- service/index.js
+++ service/index.js
@@ -73,125 +73,18 @@ secureApiRouter.use(async (req, res, next) => {
   }
 });
 
-// GetTeeTimes get a list of tee times
-apiRouter.get('/teeTimes', (_req, res) => {
-    res.send(teeTimeData);
-})
-
-const teeTimeData = { data: [
-    {
-        id: 1,
-        course: 'Timpanogos Golf Club',
-        link: 'https://www.timpanogosgolf.com',
-        date: 'October 5',
-        time: '9:30am',
-        price: '$40',
-        golfers: '2-4 golfers',
-        weather: 'Loading...',
-        img: 'https://www.timpanogosgolf.com/wp-content/uploads/2022/01/Photo-Dec-05-8-30-41-PM-scaled.jpg',
-        status: 'Reserved',
-        latitude: '40.20394',
-        longitude: '-111.64808'
-    },
-    {
-        id: 2,
-        course: 'The Links at Sleepy Ridge',
-        link: 'https://www.sleepyridgegolf.com',
-        date: 'October 6',
-        time: '10:35am',
-        price: '$55',
-        golfers: '1 golfer',
-        weather: 'Loading...',
-        img: 'https://golfcoursegurus.com/photos/utah/sleepyridge/large/Sleepy-Ridge-clubhouse-mountain.jpg',
-        status: 'Reserved',
-        latitude: '40.284927',
-        longitude: '-111.742004'
-    },
-    {
-        id: 3,
-        course: 'Riverside Country Club',
-        link: 'https://www.riversidecountryclub.org',
-        date: 'October 7',
-        time: '12:45pm',
-        price: '$65',
-        golfers: '2-4 golfers',
-        weather: 'Loading...',
-        img: 'https://media.licdn.com/dms/image/v2/C4D1BAQFmAYoU9h3HFg/company-background_10000/company-background_10000/0/1625267164271/riverside_country_club_provo_cover?e=2147483647&v=beta&t=gdsiAv_j3bA-YADzbZG98aX5b8HJ4d0Fqs79fy__fw4',
-        status: 'Reserved',
-        latitude: '40.2689972',
-        longitude: '-111.6570931'
-    },
-    {
-        id: 4,
-        course: 'Riverside Country Club',
-        link: 'https://www.riversidecountryclub.org',
-        date: 'October 8',
-        time: '1:15pm',
-        price: '$65',
-        golfers: '2-4 golfers',
-        weather: 'Loading...',
-        img: 'https://media.licdn.com/dms/image/v2/C4D1BAQFmAYoU9h3HFg/company-background_10000/company-background_10000/0/1625267164271/riverside_country_club_provo_cover?e=2147483647&v=beta&t=gdsiAv_j3bA-YADzbZG98aX5b8HJ4d0Fqs79fy__fw4',
-        status: 'Reserved',
-        latitude: '40.2689972',
-        longitude: '-111.6570931'
-    },
-    {
-        id: 5,
-        course: 'Timpanogos Golf Club',
-        link: 'https://www.timpanogosgolf.com',
-        date: 'October 9',
-        time: '3:30pm',
-        price: '$40',
-        golfers: '1-2 golfers',
-        weather: 'Loading...',
-        img: 'https://www.timpanogosgolf.com/wp-content/uploads/2022/01/Photo-Dec-05-8-30-41-PM-scaled.jpg',
-        status: 'Reserved',
-        latitude: '40.20394',
-        longitude: '-111.64808'
-    },
-    {
-        id: 6,
-        course: 'The Links at Sleepy Ridge',
-        link: 'https://www.sleepyridgegolf.com',
-        date: 'October 10',
-        time: '4:45pm',
-        price: '$55',
-        golfers: '1 golfer',
-        weather: 'Loading...',
-        img: 'https://golfcoursegurus.com/photos/utah/sleepyridge/large/Sleepy-Ridge-clubhouse-mountain.jpg',
-        status: 'Reserved',
-        latitude: '40.284927',
-        longitude: '-111.742004'
-    },
-    {
-        id: 7,
-        course: 'Riverside Country Club',
-        link: 'https://www.riversidecountryclub.org',
-        date: 'October 11',
-        time: '6:00pm',
-        price: '$65',
-        golfers: '2-4 golfers',
-        weather: 'Loading...',
-        img: 'https://media.licdn.com/dms/image/v2/C4D1BAQFmAYoU9h3HFg/company-background_10000/company-background_10000/0/1625267164271/riverside_country_club_provo_cover?e=2147483647&v=beta&t=gdsiAv_j3bA-YADzbZG98aX5b8HJ4d0Fqs79fy__fw4',
-        status: 'Reserved',
-        latitude: '40.2689972',
-        longitude: '-111.6570931'
-    },
-    {
-        id: 8,
-        course: 'Timpanogos Golf Club',
-        link: 'https://www.timpanogosgolf.com',
-        date: 'October 12',
-        time: '7:15pm',
-        price: '$40',
-        golfers: '1-2 golfers',
-        weather: 'Loading...',
-        img: 'https://www.timpanogosgolf.com/wp-content/uploads/2022/01/Photo-Dec-05-8-30-41-PM-scaled.jpg',
-        status: 'Reserved',
-        latitude: '40.20394',
-        longitude: '-111.64808'
+// Get Available Tee Times
+apiRouter.get('/teeTimes', async (_req, res) => {
+    try {
+      const reservedTeeTimes = await DB.getReservedTeeTimes();
+      const reservedIds = reservedTeeTimes.map((r) => r.teeTimeId);
+      const availableTeeTimes = await DB.getAvailableTeeTimes(reservedIds);
+      res.send({ data: availableTeeTimes });
+    } catch (err) {
+      console.error('Error fetching tee times:', err);
+      res.status(500).send({ msg: 'Failed to fetch tee times' });
     }
-]};
+});
 
 // Default error handler
 app.use(function (err, req, res, next) {
